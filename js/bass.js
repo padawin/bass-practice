8ecbdc5508f8e2dee10a948ffdb7bacab04fdf81
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-03-12 13:51:37 +0000

    first draft implemented

--- js/bass.js
+++ js/bass.js
@@ -6,7 +6,7 @@
  * - see the full fretboard in romance and english system (learning mode)
  */
 (function() {
-	var chords, systems, positions, difficulties;
+	var bassPractice, chords, systems, positions, difficulties;
 
 	chords = [
 		// English
@@ -32,9 +32,78 @@
 		[['E', 'Mi'],['F', 'Fa'], ['F#', 'Fa#'], ['G', 'Sol'], ['G#', 'Sol#'], ['A', 'La'], ['A#', 'La#'], ['B', 'Si'], ['C', 'Do'], ['C#', 'Do#'], ['D', 'Ré'], ['D#', 'Ré#'], ['Mi', 'Mi#']]
 	];
 
-	difficulties = {
-		english: systems.english,
-		romance: systems.romance,
-		all: systems.english + systems.romance
+	function _checkNote(note, available) {
+		return !!~available.indexOf(note);
+	}
+
+	bassPractice = function(element, mode) {
+		if (!systems[mode]) {
+			throw 'Invalid mode: ' + mode;
+		}
+
+		this.element = B.$id('board');
+		this.mode = mode;
+		this.results = {correct: 0, incorrect: 0};
 	};
+
+	bassPractice.modes = {ENGLISH: 'english', ROMANCE: 'romance', ALL: 'all'};
+
+	bassPractice.prototype.getRandomNote = function() {
+		var firstChord = systems[this.mode][0],
+			lastChord = systems[this.mode][1];
+		return chords[Math.floor(Math.random() * (lastChord - firstChord + 1)) + firstChord];
+	};
+
+	bassPractice.prototype.start = function(turns) {
+		var note, canClick = false, instance = this, totalTurns = turns;
+		B.$id('result').innerHTML = '';
+
+		function _turn() {
+			if (turns-- == 0) {
+				B.$id('result').innerHTML = 'Finished, your results are: <br />Corrects: ' + instance.results.correct
+					+ '<br /> Incorrect: ' + instance.results.incorrect;
+				return;
+			}
+
+			note = instance.getRandomNote();
+			B.$id('note-to-play').innerHTML = note;
+			canClick = true;
+			B.$id('turn').innerHTML = (totalTurns - turns) + ' out of ' + totalTurns;
+		}
+
+		function _clickTable(event) {
+			var clickedChord = event.target,
+				clickedString = clickedChord.parentNode,
+				fret = clickedChord.parentNode.children.indexOf(clickedChord) - 1,
+				string = clickedString.parentNode.children.indexOf(clickedString);
+
+			if (!canClick || clickedChord.tagName != 'TD') {
+				return false;
+			}
+
+			canClick = false;
+			if (_checkNote(note, positions[string][fret])) {
+				B.addClass('answer', 'right');
+				B.removeClass('answer', 'wrong');
+				B.$id('answer').innerHTML = 'Correct';
+				instance.results.correct++;
+			}
+			else {
+				B.addClass('answer', 'wrong');
+				B.removeClass('answer', 'right');
+				B.$id('answer').innerHTML = 'Incorrect';
+				instance.results.incorrect++;
+			}
+			_turn();
+		}
+
+		B.addEvent(this.element, 'click', _clickTable);
+		B.addEvent(B.$id('restart'), 'click', function(e) {
+			instance.start(totalTurns);
+			return e.preventDefault();
+		});
+		_turn();
+	};
+
+	window.BassPractice = bassPractice;
 })();
